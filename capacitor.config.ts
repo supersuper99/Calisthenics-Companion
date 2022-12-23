102dcebd6f2ac25832a62592f4ee1a2d80fabeb4
PAPI <Eandrews100@gmail.com>
2022-12-22 17:46:29 -0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Calisthenics-companion',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
